e7cd33ef661484388e927e0a416698ec10634aa1
Hibiki Shibata <hibikishibata1853@gmail.com>
2025-01-18 06:56:10 +0900

    extractrequireddata.kt edited

--- app/src/main/kotlin/org/dopc/ClientServerConfig/VenueSauceData/ExtractRequiredData.kt
+++ app/src/main/kotlin/org/dopc/ClientServerConfig/VenueSauceData/ExtractRequiredData.kt
@@ -8,42 +8,58 @@ import kotlinx.serialization.json.*
 
 import staticVenueInfoStructure.VenueRawStatic
 import dynamixVenueInfoStructure.VenueRawDynamic
+import dynamixVenueInfoStructure.DistanceRange
+
+
+data class VenueStaticData (
+    val coordinates: List<Double>
+)
+
+
+data class VenueDynamicData (
+    val order_minimum_no_surcharge: Int,
+    val delivery_pricing: Int,
+    val distance_ranges: List<DistanceRange>
+)
 
 
 
-class ExtractRequiredVenueInfoForDopc(){
+class ExtractRequiredVenueInfoForDopc() {
 
     val json = Json { ignoreUnknownKeys = true } // Parse json string
     val requestRestaurantData = RequestRestaurantData()
 
-    suspend fun venueCoordinatesStatic(venueSlug: String){
+    suspend fun venueCoordinatesStatic(venueSlug: String): VenueStaticData {
 
         // val staticVenueInfo = test.fetchStaticVenueInfo("home-assignment-venue-helsinki")        
-        val staticVenueInfo = requestRestaurantData.fetchStaticVenueInfo(venueSlug)
-        val staticVenueInfoJson = json.decodeFromString<VenueRawStatic>(staticVenueInfo)
-        val venueCoordinates = staticVenueInfoJson.venue_raw.location.coordinates
+        val staticVenueJsonString: String = requestRestaurantData.fetchStaticVenueInfo(venueSlug)
+        val staticVenueInfoJson = json.decodeFromString<VenueRawStatic>(staticVenueJsonString)
+        val venueCoordinates: List<Double> = staticVenueInfoJson.venue_raw.location.coordinates
         
-        println("VENUE COORDINATES IS *****:=> "+ venueCoordinates)
 
-        // val dynamicVenueInfo = requestRestaurantData.fetchDynamicVenueInfo(venueSlug)        
+        return VenueStaticData(
+            coordinates = venueCoordinates
+            )   
     }
 
 
 
-    suspend fun venueDeliveryFeesDynamic(venueSlug: String){
 
-        val dynamicVenueInfo = requestRestaurantData.fetchDynamicVenueInfo(venueSlug)
-        val dynamicVenueInfoJson = json.decodeFromString<VenueRawDynamic>(dynamicVenueInfo)
+    suspend fun venueDeliveryFeesDynamic(venueSlug: String): VenueDynamicData {
 
-        val orderMinimumNoSurcharge = dynamicVenueInfoJson.venue_raw.delivery_specs.order_minimum_no_surcharge
-        val basePrice = dynamicVenueInfoJson.venue_raw.delivery_specs.delivery_pricing.base_price
-        val distanceRanges = dynamicVenueInfoJson.venue_raw.delivery_specs.delivery_pricing.distance_ranges
+        val dynamicVenueJsonString: String = requestRestaurantData.fetchDynamicVenueInfo(venueSlug)
+        val dynamicVenueInfoJson = json.decodeFromString<VenueRawDynamic>(dynamicVenueJsonString)
 
-        println("SURCHARGE=> "+ orderMinimumNoSurcharge)
-        println("BASEPRICE=> " + basePrice)
-        println("DISTANCERANGE=> " + distanceRanges)
+        val orderMinimumNoSurcharge: Int = dynamicVenueInfoJson.venue_raw.delivery_specs.order_minimum_no_surcharge
+        val basePrice: Int = dynamicVenueInfoJson.venue_raw.delivery_specs.delivery_pricing.base_price
+        val distanceRanges: List<DistanceRange> = dynamicVenueInfoJson.venue_raw.delivery_specs.delivery_pricing.distance_ranges
 
 
+        return VenueDynamicData(
+            order_minimum_no_surcharge = orderMinimumNoSurcharge,
+            delivery_pricing = basePrice,
+            distance_ranges = distanceRanges
+        )
     }
 
 }
